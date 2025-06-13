28e40e17804e81691e957881c2f1c72ca389b9e1
AviaHillel <avia.hillel@asperii.com>
2025-06-13 13:19:12 +0300

    product class

--- src/main/java/com/example/product_api/model/Product.java
+++ src/main/java/com/example/product_api/model/Product.java
@@ -1,4 +1,97 @@
 package com.example.product_api.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    private String description;
+
+    @Column(nullable = false)
+    private Integer stockQuantity;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updatedDate;
+
+    @PrePersist
+    protected void onCreate() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdDate = now;
+        this.updatedDate = now;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedDate = LocalDateTime.now();
+    }
+
+    //constructors
+    public Product() {
+    }
+
+    public Product(String name, String description, Integer stockQuantity) {
+        this.name = name;
+        this.description = description;
+        this.stockQuantity = stockQuantity;
+    }
+
+    //getters
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Integer getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public LocalDateTime getCratedDate() {
+        return createdDate;
+    }
+
+    public LocalDateTime getUpdatedDate() {
+        return updatedDate;
+    }
+
+    //setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStockQuantity(Integer stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+
+    public void setCratedDate(LocalDateTime cratedDate) {
+        this.createdDate = cratedDate;
+    }
+
+    public void setUpdatedDate(LocalDateTime updatedDate) {
+        this.updatedDate = updatedDate;
+    }
 }
